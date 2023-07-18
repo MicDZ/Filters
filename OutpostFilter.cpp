1e02863e870a9684091fb67186747e64972d4a76
MicDZ <me@micdz.cn>
2023-07-18 17:56:54 +0800

    Init.

--- OutpostFilter.cpp
+++ OutpostFilter.cpp
@@ -1,33 +1,33 @@
 #include "OutpostFilter.h"
 
-
 //
 // Created by MicDZ on 2023/7/16.
 //
 
-ly::OutpostFilter::OutpostFilter() {
+ly::OutpostFilter::OutpostFilter()
+{
     is_kalman_init = false;
 }
 
-void ly::OutpostFilter::setTransitionMatrix() {
+void ly::OutpostFilter::setTransitionMatrix()
+{
 
     double
-    x = ekf.posteriori_state_estimate(0),
-    y = ekf.posteriori_state_estimate(1),
-    theta = ekf.posteriori_state_estimate(2),
-    velocity = ekf.posteriori_state_estimate(3),
-    acceleration = ekf.posteriori_state_estimate(4);
+        x = ekf.posteriori_state_estimate(0),
+        y = ekf.posteriori_state_estimate(1),
+        theta = ekf.posteriori_state_estimate(2),
+        velocity = ekf.posteriori_state_estimate(3),
+        acceleration = ekf.posteriori_state_estimate(4);
 
-    ekf.transition_matrix<<
-        1, 0, -1./2*sin(theta)*updatedTime, cos(theta)*updatedTime,0,
-        0, 1, -1./2*cos(theta)*updatedTime, -sin(theta)*updatedTime,0,
-        0, 0, 1, -acceleration/(velocity*velocity)*updatedTime, updatedTime/velocity,
+    ekf.transition_matrix << 1, 0, -1. / 2 * sin(theta) * updatedTime, cos(theta) * updatedTime, 0,
+        0, 1, -1. / 2 * cos(theta) * updatedTime, -sin(theta) * updatedTime, 0,
+        0, 0, 1, -acceleration / (velocity * velocity) * updatedTime, updatedTime / velocity,
         0, 0, 0, 1, 0,
         0, 0, 0, 0, 1;
-
 }
 
-void ly::OutpostFilter::setUpdatedTime(double delta_t) {
+void ly::OutpostFilter::setUpdatedTime(double delta_t)
+{
     if (fabs(delta_t) < 1e-4) // 防止时间差为0
     {
         updatedTime = 8.0 / 1000.0;
@@ -38,70 +38,66 @@ void ly::OutpostFilter::setUpdatedTime(double delta_t) {
     }
 }
 
-void ly::OutpostFilter::setMeasurementNoise() {
-    //TODO: 根据装甲板的距离设置噪声
+void ly::OutpostFilter::setMeasurementNoise()
+{
+    // TODO: 根据装甲板的距离设置噪声
 
     double
-    measurementNoiseX = 0.1,
-    measurementNoiseY = 0.1;
+        measurementNoiseX = 0.1,
+        measurementNoiseY = 0.1;
 
-    ekf.measurement_noise_cov<<
-        measurementNoiseX, 0,
+    ekf.measurement_noise_cov << measurementNoiseX, 0,
         0, measurementNoiseY;
 }
 
-
-
-void ly::OutpostFilter::setProcessNoise() {
+void ly::OutpostFilter::setProcessNoise()
+{
     double
-    processNoiseX = 0.1,
-    processNoiseY = 0.1,
-    processNoiseTheta = 0,
-    processNoiseVelocity = 0.1,
-    processNoiseAcceleration = 0.1;
+        processNoiseX = 0.1,
+        processNoiseY = 0.1,
+        processNoiseTheta = 0,
+        processNoiseVelocity = 0.1,
+        processNoiseAcceleration = 0.1;
     Eigen::Matrix<double, 5, 5> process_noise;
-    process_noise<<
-        processNoiseX, 0, 0, 0, 0,
+    process_noise << processNoiseX, 0, 0, 0, 0,
         0, processNoiseY, 0, 0, 0,
         0, 0, processNoiseTheta, 0, 0,
         0, 0, 0, processNoiseVelocity, 0,
         0, 0, 0, 0, processNoiseAcceleration;
 
     Eigen::Matrix<double, 5, 2> process_noise_matrix;
-    process_noise_matrix<<
-        1, 0, 0, 0, 0,
+    process_noise_matrix << 1, 0, 0, 0, 0,
         0, 1, 0, 0, 0,
         updatedTime, 0, 1, 0, 0,
         0, updatedTime, 0, 1, 0,
         0, 0, 0, 0, 1;
 
-
-    //FIXME: 这里的设置有点问题
+    // TODO: 这里过程噪声的设置还有一个变换
     ekf.process_noise_cov = process_noise_matrix * process_noise * process_noise_matrix.transpose();
 }
-void ly::OutpostFilter::rebootEkf(Eigen::Matrix<double, 2, 1> measurement) {
-    ekf.posteriori_state_estimate<<
-                                 measurement(0), measurement(1), 0, 0, 0;
+void ly::OutpostFilter::rebootEkf(Eigen::Matrix<double, 2, 1> measurement)
+{
+    ekf.posteriori_state_estimate << measurement(0), measurement(1), 0, 0, 0;
     ekf.error_cov_post = Eigen::Matrix<double, 5, 5>::Identity();
 }
 
-
-
-Eigen::Matrix<double, 2, 1> ly::OutpostFilter::correct(Eigen::Matrix<double, 2, 1> measurement) {
-    //setUpdatedTime(measurement(2));
+Eigen::Matrix<double, 2, 1> ly::OutpostFilter::correct(Eigen::Matrix<double, 2, 1> measurement)
+{
+    // setUpdatedTime(measurement(2));
     puts("in");
-    setProcessNoise(); // 更新过程噪声
+    setProcessNoise();     // 更新过程噪声
     setMeasurementNoise(); // 更新测量噪声
-    std::cout<<measurement<<std::endl;
+    std::cout << measurement << std::endl;
     ekf.predict(measureToState, measurement);
     ekf.update();
 
     Eigen::Vector2d result;
-    result<<ekf.posteriori_state_estimate(0), ekf.posteriori_state_estimate(1);
+    result << ekf.posteriori_state_estimate(0), ekf.posteriori_state_estimate(1);
     return result;
 }
 
-Eigen::Matrix<double, 2, 1> ly::OutpostFilter::runKalman(Eigen::Matrix<double, 2, 1> measurement, double delta_t) {
+Eigen::Matrix<double, 2, 1> ly::OutpostFilter::runKalman(Eigen::Matrix<double, 2, 1> measurement, double delta_t)
+{
     // 第一次初始化
     if (!is_kalman_init)
     {
@@ -124,5 +120,3 @@ Eigen::Matrix<double, 2, 1> ly::OutpostFilter::runKalman(Eigen::Matrix<double, 2
         return correct(measurement);
     }
 }
-
-
